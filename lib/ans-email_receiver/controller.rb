96a2b165d82cc43613076536b9adb337e40e0ff8
sakai shunsuke <sakai@ans-web.co.jp>
2012-08-16 19:13:13 +0900

    コントローラ

--- lib/ans-email_receiver/controller.rb
+++ lib/ans-email_receiver/controller.rb
@@ -4,6 +4,7 @@ module Ans::EmailReceiver
   module Controller
     def show
       Resque.enqueue "#{params[:id]}Receiver".constantize
+      render text: "ok"
     rescue NameError
       # 見つからなかった場合の NotFound エラーは ActiveRecord のものを使いまわす
       raise ActiveRecord::RecordNotFound
